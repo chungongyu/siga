5d04b97b5d1e3403b241db1978e161a154188814
chungongyu <chungongyu@gmail.com>
2016-11-30 18:40:39 +0800

    it works

--- src/bigraph_search.h
+++
@@ -1,4 +0,0 @@
-#ifndef bigraph_search_h_
-#define bigraph_search_h_
-
-#endif // bigraph_search_h_
--- src/bigraph_walk.cpp
+++
@@ -1,6 +0,0 @@
-#include "bigraph_walk.h"
-
-#include <log4cxx/logger.h>
-
-static log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("arcs.BigraphWalk"));
-
