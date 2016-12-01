f6960eeedbc542084385ef9d94a0c03ab82963a9
chungongyu <chungongyu@gmail.com>
2016-12-01 22:45:35 +0800

    revised

--- src/constant.h
+++ src/constant.h
@@ -14,20 +14,6 @@
 #define EC_EXT    ".ec"
 #define FA_EXT    ".fa"
 
-// common
-extern const char* kLogConfig;
-extern const char* kWorkDir;
-extern const size_t kKmerSize;
-
-// preprocess
-extern const size_t kBuckets;
-
-// gap_filling
-extern const int MAX_MERGE_GAP;
-extern const int MIN_EDGE_COUNT_FOR_TRAINING;
-extern const int MAX_CHOICE;
-extern const int CANDI_THRESHOLD;
-
 // comand sorting
 enum {
     kPreprocess, 
@@ -36,8 +22,6 @@ enum {
     kOverlap, 
     kAssemble, 
     kRmDup, 
-    kScaffold, 
-    kGapFill,
     kPreQC, 
     kUnkown = 1000
 };
