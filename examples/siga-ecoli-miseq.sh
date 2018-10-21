c355df5ef6ad6fd3e4cd8ea8bc00388f9eb14b32
chungongyu <chungongyu@gmail.com>
2018-10-21 13:53:15 +0800

    log

--- examples/siga-ecoli-miseq.sh
+++ examples/siga-ecoli-miseq.sh
@@ -16,11 +16,7 @@ IN2=MiSeq_Ecoli_MG1655_110721_PF_R2.fastq.gz
 #
 
 # Program paths
-SGA_BIN=sga
-BWA_BIN=bwa
-SAMTOOLS_BIN=samtools
-BAM2DE_BIN=sga-bam2de.pl
-ASTAT_BIN=sga-astat.py
+SIGA_BIN=siga
 
 # The number of threads to use
 CPU=8
@@ -36,20 +32,14 @@ MIN_OVERLAP=85
 ASSEMBLE_OVERLAP=111
 
 # Branch trim length
-TRIM_LENGTH=400
-
-# The minimum length of contigs to include in a scaffold
-MIN_CONTIG_LENGTH=200
-
-# The minimum number of reads pairs required to link two contigs
-MIN_PAIRS=10
+TRIM_LENGTH=150
 
 #
 # Dependency checks
 #
 
 # Check the required programs are installed and executable
-prog_list="$SGA_BIN $BWA_BIN $SAMTOOLS_BIN $BAM2DE_BIN $ASTAT_BIN"
+prog_list="$SIGA_BIN"
 for prog in $prog_list; do
     hash $prog 2>/dev/null || { echo "Error $prog not found. Please place $prog on your PATH or update the *_BIN variables in this script"; exit 1; }
 done 
@@ -67,7 +57,7 @@ done
 #
 
 # Preprocess the data to remove ambiguous basecalls
-$SGA_BIN preprocess --pe-mode 1 -o reads.pp.fastq $IN1 $IN2
+$SIGA_BIN preprocess --pe-mode=1 -o reads.pp.fastq $IN1 $IN2
 
 #
 # Error Correction
@@ -76,49 +66,21 @@ $SGA_BIN preprocess --pe-mode 1 -o reads.pp.fastq $IN1 $IN2
 # Build the index that will be used for error correction
 # As the error corrector does not require the reverse BWT, suppress
 # construction of the reversed index
-$SGA_BIN index -a ropebwt -t $CPU --no-reverse reads.pp.fastq
+$SIGA_BIN index -t $CPU --no-reverse reads.pp.fastq
 
 # Perform k-mer based error correction.
 # The k-mer cutoff parameter is learned automatically.
-$SGA_BIN correct -k $CORRECTION_K --learn -t $CPU -o reads.ec.fastq reads.pp.fastq
+$SIGA_BIN correct -k $CORRECTION_K --learn -t $CPU -o reads.ec.fastq reads.pp.fastq
 
 #
 # Primary (contig) assembly
 #
 
 # Index the corrected data.
-$SGA_BIN index -a ropebwt -t $CPU reads.ec.fastq
-
-# Remove exact-match duplicates and reads with low-frequency k-mers
-$SGA_BIN filter -x 2 -t $CPU reads.ec.fastq
+$SIGA_BIN index -t $CPU reads.ec.fastq
 
 # Compute the structure of the string graph
-$SGA_BIN overlap -m $MIN_OVERLAP -t $CPU reads.ec.filter.pass.fa
+$SIGA_BIN overlap -m $MIN_OVERLAP -t $CPU reads.ec.filter.pass.fa
 
 # Perform the contig assembly
-$SGA_BIN assemble -m $ASSEMBLE_OVERLAP --min-branch-length $TRIM_LENGTH -o primary reads.ec.filter.pass.asqg.gz
-
-#
-# Scaffolding
-#
-
-PRIMARY_CONTIGS=primary-contigs.fa
-PRIMARY_GRAPH=primary-graph.asqg.gz
-
-# Align the reads to the contigs
-$BWA_BIN index $PRIMARY_CONTIGS
-$BWA_BIN aln -t $CPU $PRIMARY_CONTIGS $IN1 > $IN1.sai
-$BWA_BIN aln -t $CPU $PRIMARY_CONTIGS $IN2 > $IN2.sai
-$BWA_BIN sampe $PRIMARY_CONTIGS $IN1.sai $IN2.sai $IN1 $IN2 | $SAMTOOLS_BIN view -Sb - > libPE.bam
-
-# Convert the BAM file into a set of contig-contig distance estimates
-$BAM2DE_BIN -n $MIN_PAIRS -m $MIN_CONTIG_LENGTH --prefix libPE libPE.bam
-
-# Compute copy number estimates of the contigs
-$ASTAT_BIN -m $MIN_CONTIG_LENGTH libPE.bam > libPE.astat
-
-# Build the scaffolds
-$SGA_BIN scaffold -m $MIN_CONTIG_LENGTH -a libPE.astat -o scaffolds.scaf --pe libPE.de $PRIMARY_CONTIGS
-
-# Convert the scaffolds to FASTA format
-$SGA_BIN scaffold2fasta --use-overlap --write-unplaced -m $MIN_CONTIG_LENGTH -a $PRIMARY_GRAPH -o sga-scaffolds.fa scaffolds.scaf
+$SIGA_BIN assemble -m $ASSEMBLE_OVERLAP --min-branch-length $TRIM_LENGTH -o primary reads.ec.filter.pass.asqg.gz
