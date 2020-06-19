af543bf4576e0eb3b879fb397313b8aebd4e7fce
chungongyu <chungongyu@gmail.com>
2020-06-19 14:24:19 +0800

    feat: add unit test for preprocess and indexer module

---
+++ test/index_test.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE "siga.index"
+#include <boost/test/included/unit_test.hpp>
+
+#include "alphabet.h"
+
+BOOST_AUTO_TEST_SUITE(indexer);
+
+BOOST_AUTO_TEST_CASE(Alphabet) {
+    int i = 1;
+    BOOST_CHECK(i);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
