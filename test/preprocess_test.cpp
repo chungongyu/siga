cabad76cdc6a1182956bd3287d0318dc3451a986
chaunceyyu <chaunceyyu@tencent.com>
2020-06-21 17:11:00 +0800

    fix: run test case automatically by command: make check

--- test/preprocess_test.cpp
+++ test/preprocess_test.cpp
@@ -5,9 +5,19 @@
 
 BOOST_AUTO_TEST_SUITE(preprocess);
 
-BOOST_AUTO_TEST_CASE(PrimerScreen) {
-    int i = 1;
-    BOOST_CHECK(i);
+BOOST_AUTO_TEST_CASE(PrimerScreen_contains) {
+    {
+        std::string pcr_free_a = "AATGATACGGCGACCACCGAGATCTACA";
+        BOOST_CHECK(PrimerScreen::containsPrimer(pcr_free_a));
+    }
+    {
+        std::string pcr_free_b = "GATCGGAAGAGCGGTTCAGCAGGAATGC";
+        BOOST_CHECK(PrimerScreen::containsPrimer(pcr_free_b));
+    }
+    {
+        std::string pcr_free_b = "AGATCGGAAGAGCGGTTCAGCAGGAATGC";
+        BOOST_CHECK(!PrimerScreen::containsPrimer(pcr_free_b));
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END();
